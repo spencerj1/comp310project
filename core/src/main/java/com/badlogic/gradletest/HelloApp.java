50e4e761987c8af4af8d624a90d1831341f34137
Joey Lawrance <lawrancej@wit.edu>
2014-03-20 08:44:52 -0400

    Converted images to power of two sizes that OpenGL wants.

--- core/src/main/java/com/badlogic/gradletest/HelloApp.java
+++ core/src/main/java/com/badlogic/gradletest/HelloApp.java
@@ -4,6 +4,7 @@ package com.badlogic.gradletest;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -15,7 +16,7 @@ public class HelloApp extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture(new Pixmap(Gdx.files.internal("2_of_clubs.png")));
 //		try {
 //			new FreeTypeFontGenerator(Gdx.files.internal("test.fnt"));
 //		} catch(Exception e) {
@@ -29,7 +30,7 @@ public class HelloApp extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 100, 200);
 		batch.end();
 	}
 }
