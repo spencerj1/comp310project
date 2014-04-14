27df44f640fa0a1ba777e249d5621f88dda8dbbd
spencerj1 <spencerj1@wit.edu>
2014-04-14 11:20:43 -0400

    Poker Launcher

--- desktop/src/main/java/com/badlogic/gradletest/PokerLauncher.java
+++ desktop/src/main/java/com/badlogic/gradletest/PokerLauncher.java
@@ -7,8 +7,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class PokerLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.height = config.height + 400;
-		config.width = config.width + 1000;
+		config.height = 800;
+		config.width = 1100;
 		new LwjglApplication(new PokerGame(), config);
 	}
 }
